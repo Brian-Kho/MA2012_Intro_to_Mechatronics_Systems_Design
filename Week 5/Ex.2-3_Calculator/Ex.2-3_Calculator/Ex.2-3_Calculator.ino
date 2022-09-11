a8730996d0fe342e8ad92d0c683562b2ae5d9a2b
Albert <wensinlor@gmail.com>
2022-09-11 11:33:01 +0800

    haven't done the calculator, can't figure out how to clear the lcd and print the answer
    Signed-off-by: Albert <wensinlor@gmail.com>

--- Week 5/Ex.2-3_Calculator/Ex.2-3_Calculator/Ex.2-3_Calculator.ino
+++ Week 5/Ex.2-3_Calculator/Ex.2-3_Calculator/Ex.2-3_Calculator.ino
@@ -3,6 +3,9 @@
 // include the library code:
 #include <LiquidCrystal.h>
 
+#include <string.h> 
+using namespace std;
+
 // initialize the library by associating any needed LCD interface pin
 // with the arduino pin number it is connected to
 const int rs = 6, en = 7, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
@@ -18,12 +21,15 @@ int KB_D=13;
 
 // Declare keypad layout
 char keys[] = {'1','2','3','F',
-               '4','5','6','E',
-               '7','8','9','D',
-               'A','0','B','C'};
+               '4','5','6','=',
+               '7','8','9','/',
+               '+','0','-','*'};
 
-#define NO_OF_CHAR 6
+#define NO_OF_CHAR 7
 char d[NO_OF_CHAR+1];//FIFO queue for characters to display
+
+int v1, v2;
+int ans;
 
 
 void setup(void) {
@@ -40,24 +46,59 @@ void setup(void) {
   
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
-  // Print a message to the LCD.
-  lcd.print("hello, world!");
+
+  Serial.begin(9600);
   }
 
 void loop(void) {
     //check if the keypad is hit
-    if(digitalRead(KB_DataAvailable))
-    {
+    
+    
+    if(digitalRead(KB_DataAvailable)){
+      
       KB_Read(); //read the keypad
-
+      
       // set the cursor to column 0, line 1
       // (note: line 1 is the second row, since counting begins with 0):
-      lcd.setCursor(0, 1);
+      lcd.setCursor(0, 0);
       // print the KEY being pressed:
       lcd.print(d);
-      delay(1000);
+      delay(800);
     }
+      
+      
+    
 }
+
+void calculator(){
+  lcd.clear();
+  int i;
+  for(i=0; i<NO_OF_CHAR-1; i++){
+   
+    if (d[i] == '+' || d[i] == '-' || d[i] == '*' || d[i] == '/'){
+      int j;
+      for(j=0; j<i; j++){
+        v1 *= 10;
+        v1 = v1 + (d[j] - 48);
+      }
+      int k;
+      for(k=i+1; k<NO_OF_CHAR; k++){
+        v2 *= 10;
+        v2 = v2 + (d[k] - 48);  
+      }
+      ans = v1 + v2;
+      lcd.print(ans);
+      
+    }
+    else{
+      continue;  
+    }
+  }
+  Serial.println(v1);
+  Serial.println(v2);
+  
+}
+
 void KB_Read() {
   int ka,kb,kc,kd,i;
   char k;
@@ -67,8 +108,22 @@ void KB_Read() {
   kd=digitalRead(KB_D); //read encoder output D
 
   k=ka+kb*2+kc*4+kd*8; // combine the encoder outputs 
+
+  if(k == 7){
+    lcd.clear(); 
+    int i;
+    for(i=0; i<NO_OF_CHAR-1; i++){
+      d[i]--;
+    }
+    lcd.setCursor(0, 0);
+    calculator();
+
+  }
+  else{
+    for(i=0;i<(NO_OF_CHAR-1);i++) d[i]=d[i+1];//move displayed characters in FIFO queue forward discarding the first one
+    d[NO_OF_CHAR-1]=keys[k]; // update the key into the queue
+    d[NO_OF_CHAR]=0; // end with NULL
+  }
   
-  for(i=0;i<(NO_OF_CHAR-1);i++) d[i]=d[i+1];//move displayed characters in FIFO queue forward discarding the first one
-  d[NO_OF_CHAR-1]=keys[k]; // update the key into the queue
-  d[NO_OF_CHAR]=0; // end with NULL
+ 
 }
